09d6fd4e610884faf983c77880069264a2465dd1
Adam <adamtls9@gmail.com>
2022-09-21 19:40:48 +0200

    js first checkClick

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const prepareDOMElements = () => {
 
 const prepareDOMEvents = () => {
     addBtn.addEventListener('click', addNewTodo)
+    ulList.addEventListener('click', checkClick)
 }
 
 const addNewTodo = () => {
@@ -56,5 +57,17 @@ const createToolsArea = () => {
     toolsPanel.append(completeBtn, editBtn, deleteBtn)
 
 }
+
+const checkClick = e => {
+    if(e.target.matches('.complete')) {
+        e.target.closest('li').classList.toggle('completed')
+        e.target.classList.toggle('completed')
+    } else if(e.target.matches('.edit')) {
+        console.log('edit');
+    } else if(e.target.matches('.delete')) {
+        console.log('delete');
+    }
+}
+
 
 document.addEventListener('DOMContentLoaded', main)
